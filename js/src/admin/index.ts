deb48321b82bf3e3393eb3530fbb448a0119edd7
dashka <dashka.gb@gmail.com>
2026-04-05 00:27:23 +0800

    npm install && npm run build

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -3,5 +3,5 @@ import app from 'flarum/admin/app';
 export { default as extend } from './extend';
 
 app.initializers.add('dashzeveg-custom-head', () => {
-  console.log('[dashzeveg/flarum-custom-head] Hello, admin!');
+  // console.log('[dashzeveg/flarum-custom-head] Hello, admin!');
 });
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -3,5 +3,5 @@ import app from 'flarum/forum/app';
 export { default as extend } from './extend';
 
 app.initializers.add('dashzeveg-custom-head', () => {
-  console.log('[dashzeveg/flarum-custom-head] Hello, forum!');
+  // console.log('[dashzeveg/flarum-custom-head] Hello, forum!');
 });
