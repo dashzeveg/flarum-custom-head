731e5287c1b0907fbe510ee0567b73fd4ce2ba5a
dashka <dashka.gb@gmail.com>
2026-04-06 20:57:11 +0800

    Added Meta Pixel #2

--- extend.php
+++ extend.php
@@ -28,7 +28,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-55XTWDH9');</script>
 <!-- End Google Tag Manager -->";
-            $document->head[] = `<!-- Meta Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '2191814514906821'); fbq('track', 'PageView');</script><noscript> <img height="1" width="1" src="https://www.facebook.com/tr?id=2191814514906821&ev=PageView&noscript=1"/></noscript><!-- End Meta Pixel Code -->`;
+            $pixelCode = "<!-- Meta Pixel Code --><script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '2191814514906821'); fbq('track', 'PageView');</script><noscript> <img height=\"1\" width=\"1\" src=\"https://www.facebook.com/tr?id=2191814514906821&ev=PageView&noscript=1\"/></noscript><!-- End Meta Pixel Code -->";
+
+            $document->head[] = $pixelCode;
         }),
     (new Extend\Frontend('admin'))
         ->js(__DIR__ . '/js/dist/admin.js')
